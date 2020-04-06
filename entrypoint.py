338a67b2970b9ea8704164ac5ab22ffd771f6f67
Jose Brache <jose.brache@gmail.com>
2020-04-05 23:13:49 -0400

    output string to test_algo

--- entrypoint.py
+++ entrypoint.py
@@ -33,6 +33,7 @@ if __name__ == "__main__":
         algo_name = "{}/{}".format(config_data['username'], config_data['algoname'])
 
         build_wait(api_key, api_address, algo_name, algo_hash)
+        print("Test the algorithm? {}".format(test_algo))
         if test_algo.capitalize == 'TRUE':
             with open("{}/{}".format(repo_path, "TEST_CASES.json")) as f:
                 case_data = json.load(f)
